d2bc37fc169eac25bbcf3c7dc1d80ccd8dcc983d
BakirDiyar <diyarbakirv@gmail.com>
2020-07-14 22:26:18 -0400

    borrada variable no utilizada

--- environments/env.js
+++ environments/env.js
@@ -1,8 +1,7 @@
 module.exports = {
   portLocal: 3000,
   secret: "asngxgbdgxvhhxgxngvvcxvgnx",
-  mongoose_url:
-    "mongodb+srv://diyar:api@cluster0-4tlc2.mongodb.net/exp-db?retryWrites=true&w=majority",
+  
 };
 
 
